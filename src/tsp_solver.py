df6c8dfd343a848a3db9d877cd1a213fd521cd2d
Artenida <artenidadaci@gmail.com>
2025-11-22 23:03:45 +0100

    Add time measurement for each iteration and total time

--- src/tsp_solver.py
+++ src/tsp_solver.py
@@ -1,8 +1,10 @@
 import pulp
 from subtour_detection import find_subtours, add_subtour_constraints
-
+import time
 
 def solve_tsp_constraint_generation(n, d):
+    total_start = time.time()
+
     model = pulp.LpProblem("TSP", pulp.LpMinimize)
     x = pulp.LpVariable.dicts("x", [(i, j) for i in range(n) for j in range(n) if i != j],
                               lowBound=0, upBound=1, cat="Continuous")  # relaxed TSP
@@ -18,13 +20,18 @@ def solve_tsp_constraint_generation(n, d):
     iteration = 0
     while True:
         iteration += 1 # Counts iterations
+        iter_start = time.time()
+
         status = model.solve(pulp.PULP_CBC_CMD(msg=False))
         if status != 1:
             raise RuntimeError("Solver failed")
 
         sol = [(i, j) for (i, j) in x if pulp.value(x[i, j]) > 0]
         subtours = find_subtours(sol, n)
+
+        iter_end = time.time()
         print(f"Iteration {iteration}: {len(subtours)} subtours found")
+        print(f"Time = {iter_end - iter_start:.4f} sec")
 
         if len(subtours) == 1:
             break
@@ -42,4 +49,7 @@ def solve_tsp_constraint_generation(n, d):
                 break
 
     total_cost = pulp.value(model.objective)
+    total_end = time.time()
+    print(f"Total computation time: {total_end - total_start:.4f} sec")
+
     return tour, total_cost
